a82e9fe725b0035b0276ef92f4ad846b67f1f729
w-disaster <luca.fabri1999@gmail.com>
2023-04-19 10:23:30 +0200

    feat: first version of ex 3

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -1,5 +1,7 @@
 package u06lab.code
 
+import u06lab.code.Solitaire.render
+
 object Solitaire extends App:
   def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
     val reversed = solution.reverse
@@ -11,5 +13,37 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
+  private val off: Seq[(Int, Int)] = Seq((-3, 0), (3, 0), (2, -2), (2, 2), (-2, 2), (-2, -2), (0, 3), (0, -3))
+  private val size: Int = 5
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  private def computeNewPos(pos: (Int, Int)): Seq[(Int, Int)] = pos match
+    case (x, y) => off.map((vx, vy) => (vx + x, vy + y))
+
+  private def nextPos(i: Int, initPos: (Int, Int), alreadyPos: Seq[(Int, Int)]): Unit = //Seq[(Int, (Int, Int))] =
+    val pos = for
+      v@(x, y) <- computeNewPos(initPos)
+      if !alreadyPos.contains(v)
+      if x >= 0 && x < size && y >= 0 && y < size
+    yield (i, v)
+    val res = for
+      e @ (_, p) <- pos
+    yield nextPos(i + 1, p, alreadyPos ++ Set(p))
+    //Seq.concat(pos, res.flatten)
+    println(alreadyPos ++ pos.map((_, v) => v))
+
+  private def nextPos1(initPos: (Int, Int), alreadyPos: Seq[(Int, Int)]): Seq[Seq[(Int, Int)]] =
+    val pos = for
+      v@(x, y) <- computeNewPos(initPos)
+      if !alreadyPos.contains(v)
+      if x >= 0 && x < size && y >= 0 && y < size
+    yield nextPos1(v, alreadyPos ++ Set(v))
+    pos match
+      case List() => Seq(alreadyPos)
+      case _ => pos.flatten
+
+  for
+    x <- 0 until size
+    y <- 0 until size
+    ss <- nextPos1((x, y), Seq((x, y))).filter(s => s.size == 25)
+  yield println(render(ss, size, size) + "\n")
+
